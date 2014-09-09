283b97c0e95ee56ad8cd1554baf0e723c87a7211
stanley zheng <dev.stanzheng@gmail.com>
2014-09-09 09:33:36 -0400

    renamed route to schools

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var db = require('./data/connect.js');
 //Utilities
 var _ = require('lodash');
 var r = require('request');
@@ -11,7 +13,7 @@ var twilio = require('twilio');
 
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var schools = require('./routes/schools');
 
 var app = express();
 
@@ -27,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/schools', schools);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
