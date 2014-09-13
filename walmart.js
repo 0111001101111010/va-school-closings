636fa234534780ac2e8136a81e610aff26ea8bb1
stanley zheng <dev.stanzheng@gmail.com>
2014-09-13 18:59:13 -0400

    all hail the mullet

---
+++ walmart.js
@@ -0,0 +1,22 @@
+var Hapi = require('hapi');
+
+// Create the Walmart Labs Hapi Server
+var PORT = process.env.PORT || 8000
+var server = new Hapi.Server(PORT);
+
+server.route({
+  method: 'GET',
+  path: '/{path*}',
+  handler: {
+    directory: {
+      path: './public',
+      listing: false,
+      index: true
+    }
+  }
+});
+
+// Start your Mullet Server
+server.start(function () {
+  console.log('The Mullet Stack is running on port:', PORT);
+});
